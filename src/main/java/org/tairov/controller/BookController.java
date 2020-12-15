3aaca5e3d9c21562ba395f3d9240970f5887e65e
Bubakasanek <sanek.tai0@gmail.com>
2020-12-15 16:27:57 +0400

    Work version

--- src/main/java/org/tairov/controller/BookController.java
+++ src/main/java/org/tairov/controller/BookController.java
@@ -1,5 +1,5 @@
 package org.tairov.controller;
-
+//
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -9,18 +9,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.tairov.model.Book;
 import org.tairov.service.BookHolder;
-
+//
 @Controller
 public class BookController {
 
 //    @Autowired
 //    private BookHolder bookHolder;
-//
+////
 //    @RequestMapping(value = "/")
 //    public String index() {
 //        return "index";
 //    }
-
+//
     @RequestMapping(value = "books", method = RequestMethod.GET)
     public String bookList(Model model) {
 //        model.addAttribute("book", new Book());
@@ -28,7 +28,7 @@ public class BookController {
 
         return "books";
     }
-
+//
 //    @RequestMapping(value = "/books/add", method = RequestMethod.POST)
 //    public String addBook(@ModelAttribute("book") Book book) {
 //        if (book.getId() == 0) {
@@ -40,14 +40,14 @@ public class BookController {
 //
 //        return "redirect:/books";
 //    }
-//
+//////
 //    @RequestMapping(value = "books/remove/{id}", method = RequestMethod.GET)
 //    public String removeBook(@PathVariable("id") int id) {
 //        bookHolder.removeBook(id);
 //
 //        return "redirect:/books";
 //    }
-//
+//////
 //    @RequestMapping(value = "books/edit/{id}")
 //    public String editBook(@PathVariable("id") int id, Model model) {
 //        model.addAttribute("book", bookHolder.getBookById(id));
@@ -55,5 +55,5 @@ public class BookController {
 //
 //        return "books";
 //    }
-
+//
 }
--- target/SpringSimple1-1.0-SNAPSHOT/index.jsp
+++ target/SpringSimple1-1.0-SNAPSHOT/index.jsp
@@ -17,3 +17,9 @@
 <br/>
 </body>
 </html>
+
+<%--<%@ page contentType="text/html;charset=UTF-8" language="java" %>--%>
+<%--<html>--%>
+<%--<head><title>Spring</title></head>--%>
+<%--<body> This my first Spring application!!!!</body>--%>
+<%--</html>--%>
